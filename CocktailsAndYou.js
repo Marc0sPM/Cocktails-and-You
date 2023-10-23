3ba75401469117a97cfc8c5f89df8a1d711f9651
Marc0sPM <marcospm0306@gmail.com>
2023-10-23 04:34:04 +0200

    botones con funciones

---
+++ CocktailsAndYou.js
@@ -0,0 +1,8 @@
+
+function mostrarOcultar(parrafoId) {
+    var parrafos = document.querySelectorAll('.contenido');
+    parrafos.forEach(function(parrafo) {
+        if(parrafo.id === parrafoId) parrafo.style.display = "block";
+        else parrafo.style.display = "none";
+    });
+}
