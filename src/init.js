2d18be916daafd70c0a6660ebbd14b04cb51171c
Vivu24 <93614868+Vivu24@users.noreply.github.com>
2023-10-26 20:56:16 +0200

    Probando propiedades de sprites
    Spoiler, no carga bien la imagen

--- src/init.js
+++ src/init.js
@@ -14,14 +14,19 @@ var game = new Phaser.Game(config);
 
 function preload(){
 
-    this.load.image("player", "./assets/sprites/player.png");
+    //this.load.image("player", "./assets/sprites/player.png");
+    this.load.image("player", ".\assets\sprites\player.png")
 }
 
 function create(){
 
-    this.player = this.add.image(50, 50, "player");
+    this.player = this.add.image(100, 100, "player");
     this.player.setScale(2);
     this.player.flipX = true;
+    this.player.setAngle(45);
+    this.player.setOrigin(0,1);
 }
 function update(time, delta){
+    this.player.angle++;
+    this.player.x++;
 }
