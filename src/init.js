47f672065a8485cf2a991912a405a2c52a6f2021
Mikeey84 <milope21@ucm.es>
2023-11-03 23:54:22 +0100

    animaciones de stay y andar completadas aunque falta revisar los cambios de escala

--- src/init.js
+++ src/init.js
@@ -17,6 +17,10 @@ class MainScene extends Phaser.Scene {
       this.load.spritesheet('player',
                      '..\\assets\\sprites\\Player\\sprites-walk.png',
                       { frameWidth: 384/8, frameHeight: 48 });
+
+      this.load.spritesheet('player_stay', 
+                      '..\\assets\\sprites\\Player\\sprites-idle.png',
+                       {frameWidth: 192/4, frameHeight: 48});
     }
     
     create(){
@@ -27,43 +31,55 @@ class MainScene extends Phaser.Scene {
 
       
       // player = new Player(this, 100, 100, "player");   //Constructora del player (HAY QUE VERLO)
-      player = this.add.image(100,100, "player");
+      player = this.add.sprite(100,100, "player");
       player.setScale(2);
 
 
          this.anims.create({
           key: "walk",
           frames: this.anims.generateFrameNumbers("player", { start: 0, end: 7 }), // Cambia estos números según tus frames
-           frameRate: 10, // Velocidad de la animación
+           frameRate: 15, // Velocidad de la animación
            repeat: -1 // -1 para bucle infinito
+          
+          
         });
+        this.anims.create({
+          key:"stay",
+          frames: this.anims.generateFrameNumbers("player_stay", {start: 0, end: 3}),
+          frameRate: 7,
+          repeat: -1,
+        });
+        
       
     }
     update(time, delta){
       const cursors = this.input.keyboard.createCursorKeys();
-
+      const noInput = !cursors.up.isDown && !cursors.down.isDown && !cursors.left.isDown && !cursors.right.isDown;
       // Mover hacia arriba
   if (cursors.up.isDown) {
-    player.y -= 5;
+    player.y -= 3;
   }
   // Mover hacia abajo
   else if (cursors.down.isDown) {
-    player.y += 5;
+    player.y += 3;
   }
 
   // Mover hacia la izquierda
   if (cursors.left.isDown) {
-    player.x -= 5;
+    player.setScale(-2,2);
+    player.x -= 3;
   }
   // Mover hacia la derecha
   else if (cursors.right.isDown) {
-    player.x += 5;
+    player.setScale(2,2);
+    player.x += 3;
   }
-  // if (!cursors.up.isDown && !cursors.down.isDown && !cursors.left.isDown && !cursors.right.isDown) {
-  //   player.anims.stop();
-  // } else {
-  //   player.anims.play("walk", true);
-  // }
+  if(noInput){
+    player.anims.play("stay", true);
+  }
+  else {
+    player.anims.play("walk", true);
+   }
     }
 
   }
---
+++ src/scenes/MainScene.js
@@ -0,0 +1,18 @@
+export default class Menu extends Phaser.Scene {
+
+    constructor() {
+      // Nombre de la escena para el SceneManager
+      super({ key: 'menu-principal' }); 
+    }
+  
+    // Métodos init, preload, create, update
+    init(){
+
+    }
+    preload(){
+        
+    }
+    create(){
+        
+    }
+  }  
