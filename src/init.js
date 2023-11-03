6d720988749b007c912ceb6c58ee8611df419746
Vivu24 <93614868+Vivu24@users.noreply.github.com>
2023-11-03 09:35:11 +0100

    Hay movimiento al menos, se considera esto demo?

--- src/init.js
+++ src/init.js
@@ -1,4 +1,6 @@
 
+let player;
+let background;
 
 class MainScene extends Phaser.Scene {
 
@@ -11,15 +13,54 @@ class MainScene extends Phaser.Scene {
   
     preload(){
       console.log("Soy preload");
-        
+      this.load.image("background", "..\\assets\\sprites\\FondoTemporal.jpg");
+      this.load.image("player", "..\\assets\\sprites\\PersonajeMovPrueba.png");
     }
     
     create(){
       console.log("Soy create");
+      background = this.add.image(0,0, "background")
+      background.setOrigin(0, 0); // Ajusta el origen de la imagen a la esquina superior izquierda
+      background.setScale(800 / background.width, 600 / background.height); // Ajusta la escala para que llene la pantalla
+        player = this.add.image(100,100, "player");
+
+        player.setScale(0.1);
+
+        // this.anims.create({
+        //   key: "walk",
+        //   frames: this.anims.generateFrameNumbers("player", { start: 0, end: 1 }), // Cambia estos números según tus frames
+        //   frameRate: 10, // Velocidad de la animación
+        //   repeat: -1 // -1 para bucle infinito
+        // });
         
+        // // Inicia la animación
+        // player.play("walk");
     }
     update(time, delta){
+      const cursors = this.input.keyboard.createCursorKeys();
 
+      // Mover hacia arriba
+  if (cursors.up.isDown) {
+    player.y -= 5;
+  }
+  // Mover hacia abajo
+  else if (cursors.down.isDown) {
+    player.y += 5;
+  }
+
+  // Mover hacia la izquierda
+  if (cursors.left.isDown) {
+    player.x -= 5;
+  }
+  // Mover hacia la derecha
+  else if (cursors.right.isDown) {
+    player.x += 5;
+  }
+  // if (!cursors.up.isDown && !cursors.down.isDown && !cursors.left.isDown && !cursors.right.isDown) {
+  //   player.anims.stop();
+  // } else {
+  //   player.anims.play("walk", true);
+  // }
     }
 
   }
