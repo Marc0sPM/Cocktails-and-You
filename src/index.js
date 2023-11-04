86a5b100d4c872be3d489a1d1127954602a3794a
Marc0sPM <marcospm0306@gmail.com>
2023-11-04 01:17:47 +0100

    mas cositas

--- src/index.js
+++ src/index.js
@@ -5,3 +5,10 @@ function mostrarOcultar(parrafoId) {
         else parrafo.style.display = "none";
     });
 }
+function mostrarOcultarControles(parrafoId){
+    var parrafos = document.querySelectorAll('.minijuegos');
+    parrafos.forEach(function(parrafo) {
+        if(parrafo.id === parrafoId) parrafo.style.display = "block";
+        else parrafo.style.display = "none";
+    });
+}
